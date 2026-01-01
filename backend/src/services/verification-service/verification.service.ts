4f1ba85595ad73b0bd659deba7423d57a172b3bf
Manoj Kumar <kvssmk36@gmail.com>
2026-01-01 23:19:37 +0530

    fix: use premade client

--- backend/src/services/verification-service/verification.service.ts
+++ backend/src/services/verification-service/verification.service.ts
@@ -1,4 +1,4 @@
-import type { PrismaClient } from '@prisma/client';
+import type { PrismaClient } from '@pinnacle/types';
 
 import prisma from '../../db/client';
 import { ValidationError, NotFoundError } from '../../types/errors.types';
